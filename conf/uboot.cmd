2e601ae436ff996b2dd08674e1ac935cafe8e1fb
Huaqi Fang <578567190@qq.com>
2021-03-02 13:36:54 +0800

    conf: update configuration due linux/uboot/opensbi changes
    IP driver now switch to use Nuclei driver
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/uboot.cmd
+++ conf/uboot.cmd
@@ -4,6 +4,5 @@ echo 'Loading ramdisk'
 fatload mmc 0 0xa8300000 uInitrd.lz4
 echo 'Loading dtb'
 fatload mmc 0 0xa8000000 kernel.dtb
-setenv bootargs 'earlycon=sbi'
 echo 'Starts booting from SD'
 bootm 0xa1000000 0xa8300000 0xa8000000
