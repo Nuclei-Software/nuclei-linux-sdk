dc32b46ce82c10290a18521450880e07aef7a84c
Huaqi Fang <578567190@qq.com>
2022-07-05 14:20:23 +0800

    conf: update uboot cmd to ignore dtb load if kernel.dtb not exist
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/evalsoc/uboot.cmd
+++ conf/evalsoc/uboot.cmd
@@ -2,7 +2,11 @@ echo 'Loading kernel'
 fatload mmc 0 0xa1000000 uImage.lz4
 echo 'Loading ramdisk'
 fatload mmc 0 0xa8300000 uInitrd.lz4
-echo 'Loading dtb'
-fatload mmc 0 0xa8000000 kernel.dtb
+if test -e mmc 0 kernel.dtb ; then
+    echo 'Loading dtb'
+    fatload mmc 0 0xa8000000 kernel.dtb
+else
+    echo 'kernel.dtb not found, ignore it'
+fi
 echo 'Starts booting from SD'
 bootm 0xa1000000 0xa8300000 0xa8000000
