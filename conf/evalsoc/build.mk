cfe26627ede9afb0908351f845e0f29eda6be7dc
Huaqi Fang <578567190@qq.com>
2022-08-24 17:52:10 +0800

    conf: comment about qemu currently not support evalsoc
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/evalsoc/build.mk
+++ conf/evalsoc/build.mk
@@ -1,4 +1,5 @@
 UIMAGE_AE_CMD := -a 0xA0400000 -e 0xA0400000
+# qemu currently not work for evalsoc
 QEMU_MACHINE_OPTS := -M nuclei_u,download=flashxip -smp 8 -m 1.5G
 # eg. $(confdir)/amp/cx.bin
 CORE1_APP_BIN :=
