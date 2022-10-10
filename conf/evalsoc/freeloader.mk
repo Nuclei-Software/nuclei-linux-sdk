17fb9b762ef01350bb196f076b2932d8858beb5b
Huaqi Fang <578567190@qq.com>
2022-10-10 15:47:17 +0800

    bump freeloader and evalsoc freeloader.mk to support prefetch setting
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/evalsoc/freeloader.mk
+++ conf/evalsoc/freeloader.mk
@@ -3,6 +3,9 @@ FLASH_BASE ?= 0x20000000
 FLASH_SIZE ?= 32M
 CACHE_CTRL ?= 0x100C1
 TLB_CTRL ?= 0x0
+# prefetch control register settings
+## spfl1dctrl1
+SPFL1DCTRL1 ?= 0x0
 ENABLE_SMP ?= 1
 ENABLE_L2 ?= 1
 AMPFW_START_OFFSET ?= 0x5E000000
