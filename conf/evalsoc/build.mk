980708260392738f7e3b2100cc001466048505fa
Huaqi Fang <578567190@qq.com>
2022-10-11 16:18:56 +0800

    conf: add demo usage of the INITRAMFS_PRECMD
    preramfs.sh is a script used to copy files from confdir to sysroot dir
    
    copyfiles.txt(placed in the same place of preramfs.sh) format
    
    src dst
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/evalsoc/build.mk
+++ conf/evalsoc/build.mk
@@ -1,6 +1,8 @@
 UIMAGE_AE_CMD := -a 0xA0400000 -e 0xA0400000
 # qemu currently not work for evalsoc
 QEMU_MACHINE_OPTS := -M nuclei_u,download=flashxip -smp 8 -m 1.5G
+# initramfs pre command before generate initrd ramfs
+INITRAMFS_PRECMD := bash $(confdir)/preramfs.sh $(confdir) $(buildroot_initramfs_sysroot) copyfiles.txt
 # eg. $(confdir)/amp/cx.bin
 CORE1_APP_BIN :=
 CORE2_APP_BIN :=
