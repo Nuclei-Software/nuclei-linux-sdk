aa25000cb22c5ba6449958b4a6d2d30de8a533fb
Huaqi Fang <578567190@qq.com>
2022-11-04 18:28:34 +0800

    ci: copy generated freeloader to local xl_ci place
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- .github/build_multiple_freeloader.sh
+++ .github/build_multiple_freeloader.sh
@@ -8,6 +8,7 @@ VARLIST=${VARLIST:-"16000000,100000000"}
 GITSHA=${GITSHA:-$(git describe --always)}
 MAKEOPTS=${MAKEOPTS:-""}
 DRYRUN=${DRYRUN:-0}
+SHARELOC=${SHARELOC:-/home/xl_ci/linuxsdk}
 
 # eval MAKEOPTS to overwrite variable of SOC/CORE/BOOT_MODE
 if [ "x$MAKEOPTS" !=  "x" ] ; then
@@ -17,10 +18,16 @@ fi
 
 echo "Git commit is $GITSHA"
 
+if [[ "$CI_JOB_ID" =~ ^[0-9]+$ ]] ; then
+    echo "Create sync directory"
+    SYNCDIR=$SHARELOC/$GITSHA
+    mkdir -p $SYNCDIR
+fi
+
 for var in ${VARLIST//,/ }
 do
     echo "Build freeloader for $MAKEOPTS $SELVAR=$var"
-    eval $SELVAR=$var
+    eval export $SELVAR=$var
     frlname=freeloader_${GITSHA}_${SOC}_${CORE}_${BOOT_MODE}
     if [ "$SELVAR" != "SOC" ] && [ "$SELVAR" != "CORE" ] && [ "$SELVAR" != "BOOT_MODE" ] ; then
         frlname=${frlname}_${SELVAR,,}-${var}
@@ -31,6 +38,19 @@ do
     if [ "x$DRYRUN" == "x0" ] ; then
         eval $runcmd
     fi
+    unset $SELVAR
+    if [ "x$SYNCDIR" != "x" ] ; then
+        echo "Sync freeloader $frldelf to ${SYNCDIR}"
+        cp -f $frldelf $SYNCDIR/
+    fi
 done
+
+if [ "x$SYNCDIR" != "x" ] ; then
+    echo "Link latest to $SYNCDIR"
+    rm -f $SHARELOC/latest
+    pushd $SHARELOC
+    ln -s $GITSHA latest
+    popd
+fi
 
 exit 0
