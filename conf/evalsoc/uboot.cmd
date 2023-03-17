a508fadecd6fbef1f4a00a51eb3a912c962aacac
Huaqi Fang <578567190@qq.com>
2023-03-17 18:00:49 +0800

    conf: update uboot.cmd to support bootloc var
    if bootloc set, then it will boot images from
    location provided in ${bootloc}
    
    For ci testing, you can stop uboot auto boot
    and set bootloc to another different place
    to let boot images from there
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/evalsoc/uboot.cmd
+++ conf/evalsoc/uboot.cmd
@@ -1,12 +1,18 @@
-echo 'Loading kernel'
-fatload mmc 0 0xa1000000 uImage.lz4
-echo 'Loading ramdisk'
-fatload mmc 0 0xa8300000 uInitrd.lz4
-if test -e mmc 0 kernel.dtb ; then
-    echo 'Loading dtb'
-    fatload mmc 0 0xa8000000 kernel.dtb
+test -z "${bootloc}"   && setenv bootloc .
+test -z "${kernelimg}" && setenv kernelimg ${bootloc}/uImage.lz4
+test -z "${rootfsimg}" && setenv rootfsimg ${bootloc}/uInitrd.lz4
+test -z "${dtbimg}"    && setenv dtbimg ${bootloc}/kernel.dtb
+
+echo "Boot images located in ${bootloc}"
+echo "Loading kernel: ${kernelimg}"
+fatload mmc 0 0xa1000000 ${kernelimg}
+echo "Loading ramdisk: ${rootfsimg}"
+fatload mmc 0 0xa8300000 ${rootfsimg}
+if test -e mmc 0 ${dtbimg} ; then
+    echo "Loading dtb: ${dtbimg}"
+    fatload mmc 0 0xa8000000 ${dtbimg}
 else
-    echo 'kernel.dtb not found, ignore it'
+    echo "${dtbimg} not found, ignore it"
 fi
-echo 'Starts booting from SD'
+echo "Starts booting from SD"
 bootm 0xa1000000 0xa8300000 0xa8000000
