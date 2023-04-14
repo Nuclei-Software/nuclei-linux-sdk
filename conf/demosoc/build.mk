9213b643e1f805e25431c4612ffd5028d65d31fc
Huaqi Fang <578567190@qq.com>
2023-04-14 14:29:39 +0800

    conf/demosoc: copy the preramfs.sh script from evalsoc to demosoc
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/demosoc/build.mk
+++ conf/demosoc/build.mk
@@ -1,6 +1,8 @@
 FW_TEXT_START := 0xA0000000
 UIMAGE_AE_CMD := -a 0xA0400000 -e 0xA0400000
 QEMU_MACHINE_OPTS := -M nuclei_u,download=flashxip -smp 8 -m 256M
+# initramfs pre command before generate initrd ramfs
+INITRAMFS_PRECMD := bash $(confdir)/preramfs.sh $(confdir) $(buildroot_initramfs_sysroot) copyfiles.txt
 # eg. $(confdir)/amp/cx.bin
 CORE1_APP_BIN :=
 CORE2_APP_BIN :=
