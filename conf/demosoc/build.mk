c90597c2b8da5c8cefbd6ad12071cc6510f1ecd3
Huaqi Fang <578567190@qq.com>
2023-06-05 15:42:22 +0800

    conf/demosoc: please use nuclei_demosoc for nuclei qemu 7.2
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/demosoc/build.mk
+++ conf/demosoc/build.mk
@@ -1,6 +1,6 @@
 FW_TEXT_START := 0xA0000000
 UIMAGE_AE_CMD := -a 0xA0400000 -e 0xA0400000
-QEMU_MACHINE_OPTS := -M nuclei_u,download=flashxip -smp 8 -m 256M
+QEMU_MACHINE_OPTS := -M nuclei_demosoc,download=flashxip -smp 8 -m 256M
 # initramfs pre command before generate initrd ramfs
 INITRAMFS_PRECMD := bash $(confdir)/preramfs.sh $(confdir) $(buildroot_initramfs_sysroot) copyfiles.txt
 # eg. $(confdir)/amp/cx.bin
