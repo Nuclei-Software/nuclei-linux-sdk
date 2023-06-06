58d3ad297693ce974568a7fbafa7a084754bd92b
Huaqi Fang <578567190@qq.com>
2023-06-06 14:20:00 +0800

    conf/demosoc: use qemu 7.2 to run on demosoc
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/demosoc/build.mk
+++ conf/demosoc/build.mk
@@ -1,5 +1,6 @@
 UIMAGE_AE_CMD := -a 0xA0400000 -e 0xA0400000
-QEMU_MACHINE_OPTS := -M nuclei_u,download=flashxip -smp 8 -m 256M
+# Use qemu 7.2, not work with nuclei qemu 2022.12 version
+QEMU_MACHINE_OPTS := -M nuclei_demosoc,download=flashxip -smp 8 -m 256M
 # eg. $(confdir)/amp/cx.bin
 CORE1_APP_BIN :=
 CORE2_APP_BIN :=
