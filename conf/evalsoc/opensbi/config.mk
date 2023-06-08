bbd6e02fec66ffcd1cdb7507c0b8df99cd64340f
Huaqi Fang <578567190@qq.com>
2023-06-08 17:51:13 +0800

    conf/evalsoc: update opensbi port for evalsoc rv32 bit
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/evalsoc/opensbi/config.mk
+++ conf/evalsoc/opensbi/config.mk
@@ -23,10 +23,20 @@ FW_TEXT_START ?= 0x80000000
 FW_DYNAMIC=y
 FW_JUMP=y
 
-# This needs to be 2MB aligned for 64-bit system
-FW_JUMP_ADDR=$(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x200000)))
+ifeq ($(PLATFORM_RISCV_XLEN), 32)
+  # This needs to be 4MB aligned for 32-bit system
+  FW_JUMP_ADDR=$(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x400000)))
+else
+  # This needs to be 2MB aligned for 64-bit system
+  FW_JUMP_ADDR=$(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x200000)))
+endif
 FW_JUMP_FDT_ADDR=$(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x8000000)))
 FW_PAYLOAD=y
-# This needs to be 2MB aligned for 64-bit system
-FW_PAYLOAD_OFFSET=0x200000
+ifeq ($(PLATFORM_RISCV_XLEN), 32)
+  # This needs to be 4MB aligned for 32-bit system
+  FW_PAYLOAD_OFFSET=0x400000
+else
+  # This needs to be 2MB aligned for 64-bit system
+  FW_PAYLOAD_OFFSET=0x200000
+endif
 FW_PAYLOAD_FDT_ADDR=$(FW_JUMP_FDT_ADDR)
