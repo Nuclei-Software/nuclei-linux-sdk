9774fe7b78b07cd2a95b25fd23268f7a590e9299
Huaqi Fang <578567190@qq.com>
2023-06-26 11:12:06 +0800

    conf/evalsoc: update rv64imafdc config for ccm operation
    riscv,ccm-block-size = <64>; is used to define ccm cache line
    size in bytes, for this means 64 bytes
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/evalsoc/build.mk
+++ conf/evalsoc/build.mk
@@ -1,6 +1,6 @@
 UIMAGE_AE_CMD := -a 0xA0400000 -e 0xA0400000
 # qemu currently not work for evalsoc
-QEMU_MACHINE_OPTS := -M nuclei_u,download=flashxip -smp 8 -m 1.5G
+QEMU_MACHINE_OPTS := -M nuclei_evalsoc,download=flashxip -smp 8 -m 1.5G
 # initramfs pre command before generate initrd ramfs
 INITRAMFS_PRECMD := bash $(confdir)/preramfs.sh $(confdir) $(buildroot_initramfs_sysroot) copyfiles.txt
 # eg. $(confdir)/amp/cx.bin
