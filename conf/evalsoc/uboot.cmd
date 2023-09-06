90d5abe91b4bfec6552cc47ff2dc9d6258e5afd2
Huaqi Fang <578567190@qq.com>
2023-09-06 10:22:41 +0800

    conf/evalsoc: change kernel load address to 0x83000000
    To reserve 0x830000000 - 0x80400000 = 47.75M for kernel
    decompress to avoid uncompress error issue bigger than previous
    15.75M
    
    Since rv32 linux kernel uncompressed size is about 25M,
    but rv64 is about 15M, so increase the kernel load address
    is very important for rv32 kernel.
    
    see https://gito.corp.nucleisys.com/software/linuxdev/nuclei-linux-sdk/-/issues/90
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/evalsoc/uboot.cmd
+++ conf/evalsoc/uboot.cmd
@@ -5,7 +5,7 @@ test -z "${dtbimg}"    && setenv dtbimg ${bootloc}/kernel.dtb
 
 echo "Boot images located in ${bootloc}"
 echo "Loading kernel: ${kernelimg}"
-fatload mmc 0 0x81000000 ${kernelimg}
+fatload mmc 0 0x83000000 ${kernelimg}
 echo "Loading ramdisk: ${rootfsimg}"
 fatload mmc 0 0x88300000 ${rootfsimg}
 if test -e mmc 0 ${dtbimg} ; then
@@ -15,4 +15,4 @@ else
     echo "${dtbimg} not found, ignore it"
 fi
 echo "Starts booting from SD"
-bootm 0x81000000 0x88300000 0x88000000
+bootm 0x83000000 0x88300000 0x88000000
