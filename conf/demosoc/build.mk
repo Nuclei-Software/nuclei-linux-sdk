f89d29ef8f3bd92926a2ea69699686427ccb080d
Huaqi Fang <578567190@qq.com>
2023-11-22 15:46:39 +0800

    conf: update QEMU_MACHINE_OPTS in build.mk for evalsoc/demosoc
    This need Nuclei Qemu 2023.10
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/demosoc/build.mk
+++ conf/demosoc/build.mk
@@ -1,5 +1,5 @@
 UIMAGE_AE_CMD := -a 0xA0400000 -e 0xA0400000
-# Use qemu 7.2, not work with nuclei qemu 2022.12 version
+# Need Nuclei Qemu >= 2023.10
 QEMU_MACHINE_OPTS := -M nuclei_demosoc,download=flashxip -smp 8 -m 256M
 # eg. $(confdir)/amp/cx.bin
 CORE1_APP_BIN :=
