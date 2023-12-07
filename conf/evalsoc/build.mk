841b336e620fbba74c89df67cb32cdaa449f6017
Huaqi Fang <578567190@qq.com>
2023-12-07 16:41:57 +0800

    conf/evalsoc: modify linux/uboot configurations for 6.6 and 2023.10 and gcc13
    1. RPC in glibc 2.38 is removed, so disable it
    2. Remove some packages from buildroot build due to compile fail
    3. Disable CONFIG_NET in uboot to avoid compile fail
    4. temporally enable vector in dts to test linux 6.6 vector feature
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/evalsoc/build.mk
+++ conf/evalsoc/build.mk
@@ -1,6 +1,7 @@
 FW_TEXT_START := 0x80000000
 UIMAGE_AE_CMD := -a 0x80400000 -e 0x80400000
 # Need Nuclei Qemu >= 2023.10
+ARCH_EXT := v_zfh_zvfh
 QEMU_MACHINE_OPTS := -M nuclei_evalsoc,download=flashxip -smp 8 -m 2G
 # initramfs pre command before generate initrd ramfs
 INITRAMFS_PRECMD := bash $(confdir)/preramfs.sh $(confdir) $(buildroot_initramfs_sysroot) copyfiles.txt
