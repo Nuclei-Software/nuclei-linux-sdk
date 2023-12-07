f2f7971bc709747d7477a747f57493c4eb7227b8
Huaqi Fang <578567190@qq.com>
2023-12-07 11:01:34 +0800

    conf/evalsoc: add uboot its to bring up kernel image
    Previous way is not working for address > 32bit
    
    Need a modified qemu at branch nuclei/8.0
    
    Change it like this and rebuilt it
    
    diff --git a/hw/riscv/nuclei_evalsoc.c b/hw/riscv/nuclei_evalsoc.c
    index 0dfc727b6c..83c26b6d71 100644
    --- a/hw/riscv/nuclei_evalsoc.c
    +++ b/hw/riscv/nuclei_evalsoc.c
    @@ -59,7 +59,7 @@
     #define IREGION_BASE         0x18000000
    
    -#define EVALSOC_DDR_BASE     0x80000000
    +#define EVALSOC_DDR_BASE     0x200000000
     #define EVALSOC_ILM_ADDR     0x80000000
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/evalsoc/uboot.cmd
+++ conf/evalsoc/uboot.cmd
@@ -2,17 +2,25 @@ test -z "${bootloc}"   && setenv bootloc .
 test -z "${kernelimg}" && setenv kernelimg ${bootloc}/uImage.lz4
 test -z "${rootfsimg}" && setenv rootfsimg ${bootloc}/uInitrd.lz4
 test -z "${dtbimg}"    && setenv dtbimg ${bootloc}/kernel.dtb
+test -z "${itbimg}"    && setenv itbimg ${bootloc}/image.itb
+
+if test -e mmc 0 ${itbimg} ; then
+    echo "Loading FIT Image"
+    fatload mmc 0 0x204000000 ${itbimg}
+    echo "Starts booting from SD using FIT Image"
+    bootm 0x204000000
+fi
 
 echo "Boot images located in ${bootloc}"
 echo "Loading kernel: ${kernelimg}"
-fatload mmc 0 0x83000000 ${kernelimg}
+fatload mmc 0 0x203000000 ${kernelimg}
 echo "Loading ramdisk: ${rootfsimg}"
-fatload mmc 0 0x88300000 ${rootfsimg}
+fatload mmc 0 0x208300000 ${rootfsimg}
 if test -e mmc 0 ${dtbimg} ; then
     echo "Loading dtb: ${dtbimg}"
-    fatload mmc 0 0x88000000 ${dtbimg}
+    fatload mmc 0 0x208000000 ${dtbimg}
 else
     echo "${dtbimg} not found, ignore it"
 fi
 echo "Starts booting from SD"
-bootm 0x83000000 0x88300000 0x88000000
+bootm 0x203000000 0x208300000 0x208000000
