efdc5aace1994ea771892e03eeed6c212692b816
Huaqi Fang <578567190@qq.com>
2024-03-04 12:08:47 +0800

    conf/evalsoc: add uboot spl config and freeloader support
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- conf/evalsoc/opensbi/evalsoc.c
+++ conf/evalsoc/opensbi/evalsoc.c
@@ -9,6 +9,8 @@
 
 #include <platform_override.h>
 #include <sbi/riscv_asm.h>
+#include <sbi/riscv_io.h>
+#include <sbi/sbi_console.h>
 #include <sbi_utils/fdt/fdt_helper.h>
 #include <sbi_utils/fdt/fdt_fixup.h>
 
@@ -21,6 +23,7 @@ static const struct fdt_match nuclei_evalsoc_match[] = {
 static int nuclei_evalsoc_final_init(bool cold_boot,
 				   const struct fdt_match *match)
 {
+	unsigned long smpcc_base = 0, smpcc_cfg;
 	if (cold_boot) { // Add cold boot initial steps
 	}
 
@@ -29,6 +32,26 @@ static int nuclei_evalsoc_final_init(bool cold_boot,
 		// Enable U-Mode to access all regions by setting spmpcfg0 and spmpaddr0
 		csr_write(0x1a0, 0x1f);
 		csr_write(0x1b0, 0xffffffff);
+	}
+
+	// Check mcfg_info.smp to see whether smp present
+	// if present, disable clm and enable l2 cache for boot hart
+	if (csr_read(0xfc2) & (0x1 << 11) && cold_boot) {
+		smpcc_base = csr_read(0x7f7) >> 10;
+		smpcc_base = (smpcc_base << 10) + 0x40000;
+		smpcc_cfg = readl((volatile void *)(smpcc_base + 0x4));
+		sbi_printf("SMPCC BASE=0x%lx\n", smpcc_base);
+		sbi_printf("SMPCC SMP_CFG=0x%lx\n", smpcc_cfg);
+		if (smpcc_cfg & 0x1) { // L2 Cache Present
+			sbi_printf("Disable CLM and enable L2 Cache\n");
+			// Now Cluster Local Memory is not used any more since uboot spl stage is already done
+			// We just disable this Cluster Local Memory feature and make it all L2 cache
+			// set CLM_WAY_EN = 0x0
+			writel(0, (volatile void *)(smpcc_base + 0xd8));
+			// Enable L2
+			// set CC_CTRL = 0x1
+			writel(1, (volatile void *)(smpcc_base + 0x10));
+		}
 	}
 
 	return 0;
