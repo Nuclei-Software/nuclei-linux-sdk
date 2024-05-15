4fb07b0a6d3c0f75699bae28a1d9498d04b50ed6
guibing <guibing@nucleisys.com>
2024-05-15 10:19:52 +0800

    conf: support fit image format,its not contain dts,dtb binary is placed in freeloader.
    Signed-off-by: guibing <guibing@nucleisys.com>

--- conf/evalsoc/uboot.cmd
+++ conf/evalsoc/uboot.cmd
@@ -1,18 +1,8 @@
 test -z "${bootloc}"   && setenv bootloc .
-test -z "${kernelimg}" && setenv kernelimg ${bootloc}/uImage.lz4
-test -z "${rootfsimg}" && setenv rootfsimg ${bootloc}/uInitrd.lz4
-test -z "${dtbimg}"    && setenv dtbimg ${bootloc}/kernel.dtb
+test -z "${kernelimg}" && setenv kernelimg ${bootloc}/kernel_rootfs.itb
 
 echo "Boot images located in ${bootloc}"
 echo "Loading kernel: ${kernelimg}"
 fatload mmc 0 0x83000000 ${kernelimg}
-echo "Loading ramdisk: ${rootfsimg}"
-fatload mmc 0 0x88300000 ${rootfsimg}
-if test -e mmc 0 ${dtbimg} ; then
-    echo "Loading dtb: ${dtbimg}"
-    fatload mmc 0 0x88000000 ${dtbimg}
-else
-    echo "${dtbimg} not found, ignore it"
-fi
 echo "Starts booting from SD"
-bootm 0x83000000 0x88300000 0x88000000
+bootm 0x83000000:kernel 0x83000000:ramdisk 0x88000000
