b78bb0763f3448d733f5545a38ad01adc9f9ecad
guibing <guibing@nucleisys.com>
2025-05-19 17:19:36 +0800

    conf/evalsoc: support optee.
    Signed-off-by: guibing <guibing@nucleisys.com>

--- conf/evalsoc/opensbi/evalsoc.c
+++ conf/evalsoc/opensbi/evalsoc.c
@@ -17,13 +17,21 @@ static const struct fdt_match nuclei_evalsoc_match[] = {
 	{ .compatible = "nuclei,eval-soc" },
 	{ },
 };
-
+extern void sm_init(bool cold_boot);
+extern void opteed_cpu_on_handler(uint32_t linear_id);
 static int nuclei_evalsoc_final_init(bool cold_boot,
 				   const struct fdt_match *match)
 {
-	if (cold_boot) { // Add cold boot initial steps
-	}
+	if (!cold_boot) { // Add cold boot initial steps
+        /* warm boot to setup optee ctx for secondary cpu */
+        unsigned int secondary_hartid;
 
+        secondary_hartid = current_hartid();
+        opteed_cpu_on_handler(secondary_hartid);
+
+        return 0;
+	}
+	sm_init(cold_boot);
 	// Check mcfg_info.tee to see whether tee present
 	if (csr_read(0xfc2) & 0x1) {
 		// Enable U-Mode to access all regions by setting spmpcfg0 and spmpaddr0
