e3fd417d1506e90385eac7623040c8ab29f51116
guibing <guibing@nucleisys.com>
2025-07-09 11:40:26 +0800

    conf/evalsoc: 1.support rv64/rv32 xec ethernet; 2.dts support sstc,zicbom,rv64 have svpbmt ext which is not in rv32; 3.unify linux and buildroot config.
    Signed-off-by: guibing <guibing@nucleisys.com>

--- conf/evalsoc/opensbi/evalsoc.c
+++ conf/evalsoc/opensbi/evalsoc.c
@@ -31,6 +31,19 @@ static int nuclei_evalsoc_final_init(bool cold_boot,
 		csr_write(0x1b0, 0xffffffff);
 	}
 
+	/*
+	 * If arch is rv32 or rv64 without svpbmt feature, you can use mattri to set ddr base:0xfd000000,size:0x10000 as non-cachable region.
+	 * xec dts node should contain desc_mem region from base:0xfd000000,size:0x10000; which is reserved region used to store xec descriptors.
+	 * if rv64 with svpbmt feature, xec dts node must not contain desc_mem property.
+	 */
+#if __riscv_xlen == 32
+	#define mattri1_base 0x7f5
+	#define mattri1_mask 0x7f6
+
+	csr_write(mattri1_mask, 0xffff0000);
+	csr_write(mattri1_base, 0xfd000005);
+#endif
+
 	return 0;
 }
 
