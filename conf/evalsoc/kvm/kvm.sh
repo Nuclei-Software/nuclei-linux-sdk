e75e3f4434978aa69b2e7c8d66bcce70ff6e5406
dongyongtao <dongyongtao@nucleisys.com>
2025-12-05 15:43:47 +0800

    conf/evalsoc/kvm: allocate 1900M for guest to run spec06, which is the same with fpgaci
    Signed-off-by: dongyongtao <dongyongtao@nucleisys.com>

--- conf/evalsoc/kvm/kvm.sh
+++ conf/evalsoc/kvm/kvm.sh
@@ -11,4 +11,4 @@ set -x
 mkdir -p /tmp/myshared/
 mount /dev/mmcblk0p1 /tmp/myshared/
 chmod +x lkvm-static
-lkvm-static run --9p /tmp/myshared,myshare_tag -m 1536 -c2 -k Image
+lkvm-static run --9p /tmp/myshared,myshare_tag -m 1900 -c2 -k Image
