509062eb1aee890f2524b47de6ef0fc849aad6ad
dongyongtao <dongyongtao@nucleisys.com>
2026-07-03 18:37:05 +0800

    conf/evalsoc: change to -smp 8, because qemu version from 2025.12.10 has supported detect up to 16 cpu's imisc region
    Signed-off-by: dongyongtao <dongyongtao@nucleisys.com>

--- conf/evalsoc/build.mk
+++ conf/evalsoc/build.mk
@@ -1,7 +1,7 @@
 FW_TEXT_START := 0x80000000
 UIMAGE_AE_CMD := -a 0x80400000 -e 0x80400000
 # Need Nuclei Qemu >= 2025.10
-QEMU_MACHINE_OPTS := -M nuclei_evalsoc,download=flashxip,soc-cfg=$(confdir)/evalsoc.json,aia=aplic-imsic,aia-guests=4 -smp 1 -m 2G
+QEMU_MACHINE_OPTS := -M nuclei_evalsoc,download=flashxip,soc-cfg=$(confdir)/evalsoc.json,aia=aplic-imsic,aia-guests=4 -smp 8 -m 2G
 # initramfs pre command before generate initrd ramfs
 INITRAMFS_PRECMD := bash $(confdir)/preramfs.sh $(confdir) $(buildroot_initramfs_sysroot) copyfiles.txt
 # eg. $(confdir)/amp/cx.bin
